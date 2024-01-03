9b221c4638135155a533838a822feb0f73f9af1e
MiguelBrucil <132540041+MiguelBrucil@users.noreply.github.com>
2024-01-03 10:43:10 -0500

    Proyecto de SeacrhView

---
+++ app/src/main/java/com/jmbp/chipgroup/MainActivity.java
@@ -0,0 +1,47 @@
+package com.jmbp.chipgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+    String[] codingList = {"Paul", "Milton", "Alexander", "Jeferson", "David", "Said"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.list_customtext, codingList);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("EXITO");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
